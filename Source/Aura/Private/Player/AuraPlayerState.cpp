7461323660d57d45bfceff0d96a662314e1dff81
Jerry W. Walker <jwalker@Coins>
2025-05-25 10:50:38 -0500

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// WalkerX GFL All Rights Reserved
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	SetNetUpdateFrequency(100.f);
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// WalkerX GFL All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+	
+};
