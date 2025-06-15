3554c2cc39d56811f1d3fafb16703f33c979556e
Jerry W. Walker <jwalker@Coins>
2025-06-15 12:52:20 -0500

    "Actor Effect"

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,25 @@
+// WalkerX GFL All Rights Reserved
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "Components/SphereComponent.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
