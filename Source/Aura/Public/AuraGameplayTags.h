2db5f624586a6f5f079f61d091b606cd07844537
Jerry W. Walker <jwalker@Coins>
2025-08-03 15:42:23 -0500

    Gameplay Tags Singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright WalkerX GFL, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ *  AuraGameplayTags
+ *
+ *  Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
